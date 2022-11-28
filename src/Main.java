e7648a7ca2a5eac238d8637a03740ac335e3f25b
dwwop <99904993+dwwop@users.noreply.github.com>
2022-11-28 21:14:39 +0100

    Initial setup
    Initial setup of libraries

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306", "root", "root");
+            System.out.println("Success");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
