d1b50885b598d5d2bec6a708fd417e94857e26f0
lucau <lu.cauner@gmail.com>
2022-12-08 16:56:24 +0100

    add configuration

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306", "root", "root");
-            System.out.println("Success");
+            java.util.Properties prop = new Properties();
+            prop.loadFromXML(new FileInputStream("configuration/configuration.xml"));
+            Connection connection = DriverManager.getConnection(
+                    prop.getProperty("database"),
+                    prop.getProperty("user"),
+                    prop.getProperty("password"));
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
