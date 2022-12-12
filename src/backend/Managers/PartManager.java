70de12343b389ec4dcd66fe340fc24bf92e35491
dwwop <99904993+dwwop@users.noreply.github.com>
2022-12-12 20:27:13 +0100

    Fix 0 vs Null in update

--- src/backend/Managers/PartManager.java
+++ src/backend/Managers/PartManager.java
@@ -9,6 +9,16 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class PartManager {
+    private static Double check(Double d){
+        return d == 0 ? null : d;
+    }
+    private static Short check(Short s){
+        return s == 0 ? null : s;
+    }
+
+    private static Integer check(Integer i) {
+        return i == 0 ? null : i;
+    }
     public static Part getPartByName(String partNumber) throws SQLException {
         try (PreparedStatement s = DBS.getConnection().prepareStatement("SELECT * FROM parts WHERE part_number = ?")) {
             s.setString(1, partNumber);
@@ -21,25 +31,25 @@ public class PartManager {
                 part.setPart_name_id((BigInteger) rs.getObject("part_name_id"));
                 part.setCategory_id((BigInteger) rs.getObject("category_id"));
                 part.setDrawing_id((BigInteger) rs.getObject("drawing_id"));
-                part.setRubber(rs.getShort("rubber"));
-                part.setDiameter_AT(rs.getDouble("diameter_AT"));
+                part.setRubber(check(rs.getShort("rubber")));
+                part.setDiameter_AT(check(rs.getDouble("diameter_AT")));
                 part.setDiameter_AT_tol(rs.getString("diameter_AT_tol"));
-                part.setLength_L_AT(rs.getDouble("length_L_AT"));
+                part.setLength_L_AT(check(rs.getDouble("length_L_AT")));
                 part.setLength_L_AT_tol(rs.getString("length_L_AT_tol"));
-                part.setDiameter_IT(rs.getDouble("diameter_IT"));
+                part.setDiameter_IT(check(rs.getDouble("diameter_IT")));
                 part.setDiameter_IT_tol(rs.getString("diameter_IT_tol"));
-                part.setLength_L_IT(rs.getDouble("length_L_IT"));
+                part.setLength_L_IT(check(rs.getDouble("length_L_IT")));
                 part.setLength_L_IT_tol(rs.getString("length_L_IT_tol"));
-                part.setDiameter_ZT(rs.getDouble("diameter_ZT"));
+                part.setDiameter_ZT(check(rs.getDouble("diameter_ZT")));
                 part.setDiameter_ZT_tol(rs.getString("diameter_ZT_tol"));
-                part.setLength_L_ZT(rs.getDouble("length_L_ZT"));
+                part.setLength_L_ZT(check(rs.getDouble("length_L_ZT")));
                 part.setLength_L_ZT_tol(rs.getString("length_L_ZT_tol"));
-                part.setCr_steg(rs.getInt("cr_steg"));
-                part.setCr_niere(rs.getShort("cr_niere"));
-                part.setCa(rs.getShort("ca"));
-                part.setCt(rs.getDouble("ct"));
-                part.setCk(rs.getDouble("ck"));
-                part.setRating(rs.getInt("rating"));
+                part.setCr_steg(check(rs.getInt("cr_steg")));
+                part.setCr_niere(check(rs.getShort("cr_niere")));
+                part.setCa(check(rs.getShort("ca")));
+                part.setCt(check(rs.getDouble("ct")));
+                part.setCk(check(rs.getDouble("ck")));
+                part.setRating(check(rs.getInt("rating")));
 
                 return part;
             } else {
