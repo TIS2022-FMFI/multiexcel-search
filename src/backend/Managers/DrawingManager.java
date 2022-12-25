99c711f366d70754b707ac4ae86184af37a860cd
dwwop <99904993+dwwop@users.noreply.github.com>
2022-12-25 15:22:00 +0100

    adding comments and handling of SQL Exceptions

--- src/backend/Managers/DrawingManager.java
+++ src/backend/Managers/DrawingManager.java
@@ -9,7 +9,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DrawingManager {
-
+    /**
+     * Returns id of drawing associated with input drawing if drawing exists in database.
+     * If it doesn't, insert's input drawing into database and returns associated id
+     *
+     * @param drawing - input drawing
+     * @return id of drawing
+     */
     public static BigInteger getDrawingId(byte[] drawing) {
         try (PreparedStatement s = DBS.getConnection().prepareStatement("SELECT drawing_id FROM drawings WHERE drawing = ?")) {
             s.setBytes(1, drawing);
@@ -27,20 +33,12 @@ public class DrawingManager {
         }
     }
 
-    public static BigInteger getDrawingIdFromPartName(String partName) {
-        try (PreparedStatement s = DBS.getConnection().prepareStatement("SELECT drawing_id FROM parts WHERE part_number = ?")) {
-            s.setString(1, partName);
-
-            ResultSet rs = s.executeQuery();
-            if (rs.next())
-                return BigInteger.valueOf(rs.getInt("drawing_id"));
-
-            return null;
-        } catch (SQLException ignored) {
-            return null;
-        }
-    }
-
+    /**
+     * Returns drawing based on drawing id from database
+     *
+     * @param drawingId - id of drawing
+     * @return Associated drawing or null if drawing doesn't exist
+     */
     public static Drawing getDrawing(BigInteger drawingId) {
         try (PreparedStatement s = DBS.getConnection().prepareStatement("SELECT drawing FROM drawings WHERE drawing_id = ?")) {
             s.setObject(1, drawingId);
--- src/backend/Managers/PartNameManager.java
+++ src/backend/Managers/PartNameManager.java
@@ -9,6 +9,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class PartNameManager {
+    /**
+     * Returns part name id associated with input part name from database
+     * If part name doesn't exist in database, inserts new part name and returns associated id
+     *
+     * @param partName - input part name
+     * @return associated part name id
+     */
     public static BigInteger getPartNameId(String partName) {
         if (partName == null)
             return null;
@@ -25,16 +32,23 @@ public class PartNameManager {
                 newPartName.insert();
                 return BigInteger.valueOf(newPartName.getPart_name_id());
             }
-        } catch (SQLException ignored){
+        } catch (SQLException ignored) {
             return null;
         }
     }
 
+
+    /**
+     * Returns part name based on part name id from database
+     *
+     * @param partNameId - id of part name
+     * @return Associated part name or null if part name doesn't exist
+     */
     public static Part_name getPartName(BigInteger partNameId) {
         if (partNameId == null)
             return null;
 
-        try (PreparedStatement s = DBS.getConnection().prepareStatement("SELECT part_name FROM part_names where part_name_id = ?")){
+        try (PreparedStatement s = DBS.getConnection().prepareStatement("SELECT part_name FROM part_names where part_name_id = ?")) {
             s.setObject(1, partNameId);
 
             ResultSet rs = s.executeQuery();
@@ -45,7 +59,7 @@ public class PartNameManager {
                 return partName;
             }
             return null;
-        } catch (SQLException ignored){
+        } catch (SQLException ignored) {
             return null;
         }
     }
